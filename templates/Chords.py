0e6cc707721923364a0c100ebf6ac222f8394ade
AyalaAbramson <ayala9858@gmail.com>
2026-03-06 16:30:48 +0200

    Final Version - string detection by distance calc, right chords

--- templates/Chords.py
+++ templates/Chords.py
@@ -1,33 +1,34 @@
 # --- Chord Library Data Structure ---
 # Each chord contains a list of fingers: (fret_number, string_number)
-# String 1: Top string in camera view
-# String 6: Bottom string in camera view
+# Standard Tuning Convention:
+# String 1: High E (Thinnest, bottom in standard diagrams)
+# String 6: Low E (Thickest, top in standard diagrams)
 
 CHORD_LIBRARY = {
     "C": {
-        "fingers": [(1, 5), (2, 3), (3, 2)],  # flipped strings
-        "muted": [1],
-        "open": [4, 6]
+        "fingers": [(3, 5), (2, 4), (1, 2)], # 3rd fret on 5th string, 2nd on 4th, 1st on 2nd
+        "muted": [6],
+        "open": [1, 3]
     },
     "G": {
-        "fingers": [(3, 1), (2, 2), (3, 6)],
+        "fingers": [(3, 6), (2, 5), (3, 1)], # 3rd fret on 6th string, 2nd on 5th, 3rd on 1st
         "muted": [],
-        "open": [3, 4, 5]
+        "open": [2, 3, 4]
     },
     "D": {
-        "fingers": [(2, 4), (3, 5), (2, 6)],
-        "muted": [1, 2],
-        "open": [3]
+        "fingers": [(2, 3), (3, 2), (2, 1)], # 2nd fret on 3rd string, 3rd on 2nd, 2nd on 1st
+        "muted": [5, 6],
+        "open": [4]
     },
-    "Am": {
-        "fingers": [(1, 5), (2, 3), (2, 4)],
-        "muted": [1],
-        "open": [2, 6]
+    "AM": {
+        "fingers": [(2, 4), (2, 3), (1, 2)], # 2nd fret on 4th string, 2nd on 3rd, 1st on 2nd
+        "muted": [6],
+        "open": [1, 5]
     },
     "E": {
-        "fingers": [(1, 4), (2, 2), (2, 3)],
+        "fingers": [(2, 5), (2, 4), (1, 3)], # 2nd fret on 5th string, 2nd on 4th, 1st on 3rd
         "muted": [],
-        "open": [1, 5, 6]
+        "open": [1, 2, 6]
     }
 }
 
